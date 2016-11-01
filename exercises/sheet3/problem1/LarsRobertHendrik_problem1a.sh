3bdb57241da904f61662d306d4ebc7066c4b47c2
Lars Groeber <larsgroeber7@gmail.com>
2016-11-01 10:25:58 +0100

    commented sheet3/problem1

--- exercises/sheet3/problem1/LarsRobertHendrik_problem1a.sh
+++ exercises/sheet3/problem1/LarsRobertHendrik_problem1a.sh
@@ -1,20 +1,28 @@
 #! /bin/bash -e
 
+#### GLOBALS ####
+
 prefix="data_file_00"
 suffix=".dat"
 
+#### END GLOBALS ####
+
 echo "Creating files now."
 
+# loop form 0 to 10
 for i in {0..10}; do
+    # if $i is smaller than 10 we need an extra 0
 	if test $i -lt 10; then
 		fileName=$prefix"0"$i$suffix
 	else
 		fileName=$prefix$i$suffix
 	fi
+    # create file
 	touch  $fileName
 	echo "Created file $fileName"
 done
 
+# ask user to continue
 echo "All files were created. Continuing renaming them? [Y/n]"
 read a
 if test -z $a || test $a == "Y" || test $a == "y"; then 
@@ -23,6 +31,7 @@ else
 	exit 0
 fi
 
+# renaming files: rename EXPRESSION REPLACEMENT FILE...
 rename -v _file '' data*.dat
 
 echo "Done"
--- exercises/sheet3/problem1/LarsRobertHendrik_problem1b.sh
+++ exercises/sheet3/problem1/LarsRobertHendrik_problem1b.sh
@@ -2,13 +2,15 @@
 
 # uses bash for easier control over variables and messages
 
+# check if file was given by parameter
 if test ! -z $1; then
 	inputFile=$1
 else
+    # otherwise use default one
 	inputFile="matrix.dat"
 fi
 
-# first check if file exists
+# check if file exists
 if test ! -e $inputFile && test ! -r $inputFile; then
 	echo "File $inputFile does not exist or is not readable!" >&2
 	exit 1
@@ -16,25 +18,30 @@ fi
 
 echo "Press <ENTER> to continue after one operation."
 
+# print the inputfile
 echo "This is the inputFile:"
 awk ' { print $0 }' $inputFile
 read
 
+# mean value of each line
 echo "Computing the mean value for each line."
 # NF returns the number of fields
 awk '{ print ($1+$2+$3+$4+$5)/NF }' $inputFile
 read
 
+# mean value if first column
 echo "Computing the mean value for the first column."
 # NR returns the row-number
 awk '{ total=total+$1 } END { print total/NR }' $inputFile
 read
 
+# rows with mean value of >= 1 and <= 50
 echo "Printing only rows with a mean value of >= 1 and <= 50."
 awk '{ mean=($1+$2+$3+$4+$5)/NF; if ( mean >= 1 && mean <= 50 ) \
 	print "Line "NR", mean "mean":\t" $0 }' $inputFile
 read
 
+# calculating trace
 echo "Printing the trace."
 awk '{ total=total+$NR } END { print total }' $inputFile
 
