e8d176c1e93b3cee2f6583bb93f815170333ddea
Hendrik <jjosh+github@live.de>
2016-11-08 14:43:50 +0100

    added problem 2

---
+++ exercises/sheet4/problem2/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+// (i) ﬁlling the array with arbitrary values
+// (ii) multiply each element by a constant
+// (iii) printing out the values formatted.
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+
+
+// Additionally write a function void printArray(int arr[], int len) that takes a one-dimensional array and its length.
+// Then call the function giving one row of the previously deﬁned matrix as an argument.
+void printArray(int arr[], int len) {
+    cout << "row from matrix" << endl;
+}
