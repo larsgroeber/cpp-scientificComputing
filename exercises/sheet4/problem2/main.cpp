e71cb418139af0d8da17eea90a51c1f01887a1f4
Hendrik <jjosh+github@live.de>
2016-11-08 16:00:20 +0100

    problem 2 at 90% the last method is still missing

--- exercises/sheet4/problem2/main.cpp
+++ exercises/sheet4/problem2/main.cpp
@@ -1,19 +1,60 @@
 #include <iostream>
+#include <cstdlib>
+#include <cstdio>
 
 using namespace std;
 
-// (i) ﬁlling the array with arbitrary values
-// (ii) multiply each element by a constant
-// (iii) printing out the values formatted.
+const int m = 5;                        // rows
+const int n = 10;                       // columns
+const int multFac = 2;                  // factor to multiply the matrix with
+int arr[m][n];
+int arr2[];
+
+void fillArr (int arr[][n]);
+void multArr (int arr[][n]);
+void showArr (int arr[][n]);
+void printArray(int arr2[], int len);
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    fillArr(arr);
+    cout << endl << "m x n Matrix" << endl << endl;
+    showArr(arr);
+    multArr(arr);
+    cout << endl << "m x n Matrix multiplied by " << multFac << endl << endl;
+    showArr(arr);
     return 0;
 }
 
 
+void fillArr (int arr[][n]){
+    for (int i = 0; i < m; ++i) {      //sizeof(arr)/sizeof(arr[0][3])
+        for (int j = 0; j < n; ++j) {
+            arr[i][j] = rand()%10;
+        }
+    }
+}
+
+void multArr(int arr[m][n]) {
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            arr [i][j] = arr [i][j] * multFac;
+        }
+    }
+};
+
+void showArr(int arr[m][n]) {
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            printf("|%2d" , arr [i][j]);
+        }
+        cout << "|" << endl;
+    }
+}
+
 
 // Additionally write a function void printArray(int arr[], int len) that takes a one-dimensional array and its length.
 // Then call the function giving one row of the previously deﬁned matrix as an argument.
-void printArray(int arr[], int len) {
+void printArray(int arr2[], int len) {
     cout << "row from matrix" << endl;
+    
 }
