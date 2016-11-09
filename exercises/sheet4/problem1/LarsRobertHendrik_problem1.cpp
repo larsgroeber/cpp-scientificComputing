41ca0a07a53f4806525254c7076c4a2b898479c2
Hendrik <jjosh+github@live.de>
2016-11-09 19:05:51 +0100

    renamed problem 2 added comments to problem 1a added problem 1b

--- exercises/sheet4/problem1/LarsRobertHendrik_problem1.cpp
+++ exercises/sheet4/problem1/LarsRobertHendrik_problem1.cpp
@@ -2,25 +2,52 @@
 
 using namespace std;
 
+int myfunc(int* firstIntPtr, int* secondIntPtr);
 
 int main ()
 {
-    int* intPtr;
-    double* doublePtr;
-    long double* ldoublePtr;
+    //problem 1a
+    // pointer for 3 different data types that should  point to arrays
+    int*            integerPtr;
+    double*         double_Ptr;
+    long double*    ldoublePtr;
 
+    //those are the corresponding arrays (see above)
     int intArr[] = { 1,2,3,4 };
     double doubleArr[] = { 1.5, 2.5, 3.5, 4.5285547,5,5,5,5,5 };
     long double ldoubleArr[] = { 1.25, 2.25, 3.25, 4.3785675682356 };
 
-    intPtr = intArr;
-    doublePtr = doubleArr;
+    //variabes needed for 1b
+    int x = 7;
+    int y = 3;
+    int* xPtr = &x;
+    int* yPtr = &y;
+
+    //assigning the arrays to the pointers
+    integerPtr = intArr;
+    double_Ptr = doubleArr;
     ldoublePtr = ldoubleArr;
 
-    cout << "intPtr = " << intPtr << ", *intPtr = " << *intPtr << endl;     
-    cout << "doublePtr = " << doublePtr << ", *doublePtr = " << *doublePtr << endl;     
-    // hier fehlt noch was
-    
-    cout << "intPtr++ = " << intPtr++ << ", *(intPtr++) = " << *(intPtr++) << endl;     
-    cout << "doublePtr++ = " << doublePtr++ << ", *(doublePtr++) = " << *(doublePtr++) << endl;     
+    //printing the addresses and the values the pointer are pointing at
+    cout << "addresses and the values the pointer are pointing at" << endl<<endl;
+    cout << "integerPtr = " << integerPtr << ", *integerPtr = " << *integerPtr << endl;
+    cout << "double_Ptr = " << double_Ptr << ", *double_Ptr = " << *double_Ptr << endl;
+    cout << "ldoublePtr = " << ldoublePtr << ", *ldoublePtr = " << *ldoublePtr << endl;
+
+    //incrementing the pointer and printing out the same as above
+    cout<<endl<<endl<<"incrementing the pointer gives following result:"<<endl<<endl;
+    cout << "integerPtr++ = " << integerPtr++ << ", *(integerPtr++) = " << *(integerPtr++) << endl;
+    cout << "double_Ptr++ = " << double_Ptr++ << ", *(double_Ptr++) = " << *(double_Ptr++) << endl;
+    cout << "ldoublePtr++ = " << ldoublePtr++ << ", *(ldoublePtr++) = " << *(ldoublePtr++) << endl;
+
+    cout<< endl<<endl<<"calling function from 1b:"<<endl;
+    cout << myfunc(xPtr, yPtr);
+
+
+}
+
+//problem 1b
+// multiplying two integers by using their pointers
+int myfunc(int* firstIntPtr, int* secondIntPtr){
+    return *firstIntPtr * *secondIntPtr;
 }
