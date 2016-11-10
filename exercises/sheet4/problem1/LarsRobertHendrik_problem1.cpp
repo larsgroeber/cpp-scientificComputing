16e9c125445b400298bc5c7a1cf3fe4207538cb9
Lars Gröber <larsgroeber7@gmail.com>
2016-11-10 21:42:57 +0100

    finished problem1 sheet4

--- exercises/sheet4/problem1/LarsRobertHendrik_problem1.cpp
+++ exercises/sheet4/problem1/LarsRobertHendrik_problem1.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 
 int myfunc(int* firstIntPtr, int* secondIntPtr);
+int myfunc(int& a, int& b);
+
+double scalarProduct(double* arr1, double* arr2);
+void outerProduct(double* arr1, double* arr2, double* result);
 
 int main ()
 {
@@ -39,18 +43,54 @@ int main ()
     double_Ptr++;
     ldoublePtr++;
     cout<<endl<<endl<<"incrementing the pointer gives following result:"<<endl<<endl;
-    cout << "integerPtr++ = " << integerPtr << ", *(integerPtr++) = " << *integerPtr << endl;
-    cout << "double_Ptr++ = " << double_Ptr << ", *(double_Ptr++) = " << *double_Ptr << endl;
-    cout << "ldoublePtr++ = " << ldoublePtr << ", *(ldoublePtr++) = " << *ldoublePtr << endl;
+    cout << "integerPtr = " << integerPtr << ", *integerPtr = " << *integerPtr << endl;
+    cout << "double_Ptr = " << double_Ptr << ", *double_Ptr = " << *double_Ptr << endl;
+    cout << "ldoublePtr = " << ldoublePtr << ", *ldoublePtr = " << *ldoublePtr << endl;
 
-    cout<< endl<<endl<<"calling function from 1b:"<<endl;
-    cout << myfunc(xPtr, yPtr);
+    // output of problem 1b
+    cout<< endl<<endl<<"calling function from 1b with references and x=7, y=3:"<<endl;
+    cout << myfunc(x, y) << endl;
+    cout<< "calling function from 1b with pointers:"<<endl;
+    cout << myfunc(xPtr, yPtr) << endl;
 
+    // output of problem 1c
+    cout << "\n\n" << "scalar product of (1,2,3) and (4,5,6)" << endl;
+    double arr1[] = { 1, 2, 3 };
+    double arr2[] = { 4, 5, 6 };
+    // arr1 and arr2 are pointers pointing to the arrays
+    cout << scalarProduct(arr1,arr2) << endl;
 
+    // output for problem 1d
+    cout << "\n\n" << "outer product of (1,2,3) and (4,5,6)" << endl;
+    double result[3];
+    outerProduct(arr1,arr2,result);
+    cout << "(" << result[0] << "," << result[1] << "," << result[2] << ")" << endl; 
 }
 
-//problem 1b
+//// problem 1b ////
+// multiplying two integers by using their references
+int myfunc(int& a, int& b){
+    return a * b;
+}
+
 // multiplying two integers by using their pointers
-int myfunc(int* firstIntPtr, int* secondIntPtr){
-    return *firstIntPtr * *secondIntPtr;
+int myfunc(int* a, int* b){
+    return *a * *b;
+}
+
+//// problem 1c ////
+// consider only 3-dim vectors
+double scalarProduct(double* arr1, double* arr2){
+    double result = 0;
+    for (int i=0; i < 3; ++i){
+        result += arr1[i] * arr2[i];
+    }
+    return result;
+}
+
+//// problem 1d ////
+void outerProduct(double* arr1, double* arr2, double* result){
+    for (int i=0; i < 3; ++i){
+        result[i] += arr1[(i+1)%3] * arr2[(i+2)%3] - arr1[(i+2)%3] * arr2[(i+1)%3];
+    }
 }
