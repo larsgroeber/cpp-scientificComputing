6b0d60c637e83538ff91a4d849d916ee91b0b818
Hendrik <jjosh+github@live.de>
2016-11-11 14:26:20 +0100

    created project 5 created subtask problem1

---
+++ exercises/sheet5/problem1/LarsRobertHendrik_problem1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() {
+
+    return 0;
+}
+
+// (a) Write a program that replaces every second element of an array by zero,
+// using a for loop and iterating with step size two.
+
+// (b) Write a program that prints out all odd numbers smaller than 20 by
+// using a loop and the continue statements.
+
+//(c) Write a program that calculates the largest factorial smaller than a given
+//upper limit. Therefore use a loop and the break statement to exit, when
+//the next number is larger than the limit.
+
+//(d) Binomial coeﬃcient
+//Write a program that computes the binomial coeﬃcient  (n k)
+//where n and k are speciﬁed by the user.
+
+//(e) Password prompt
+//Write a program that asks the user to enter a password. The user may
+//only enter the password a speciﬁed number of times. The program should
+//check the password every time and exit if the correct password was given
+//or the maximal limit is reached.
+
+//(f) The guessing game
+//Write program that randomly generates a number between 1 and 100 and
+//the user has to guess this number. If the user guesses too high/low the
+//program outputs ”too high/low” until the user hits the correct number.
