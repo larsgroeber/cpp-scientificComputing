728b0bcd34df30c0ab58356f165b223e9e8024cf
Hendrik <jjosh+github@live.de>
2016-11-15 11:01:31 +0100

    added 1.b

--- exercises/sheet5/problem1/LarsRobertHendrik_problem1.cpp
+++ exercises/sheet5/problem1/LarsRobertHendrik_problem1.cpp
@@ -2,51 +2,75 @@
 #include <cstdio>
 using namespace std;
 
-int intArr[21] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-int arrSize = 21;//sizeof(intArr)/ sizeof(*intArr);//sizeof(intArr) / sizeof(intArr[0]);
+const int arrSize = 21;//sizeof(intArr)/ sizeof(*intArr);//sizeof(intArr) / sizeof(intArr[0]);
+int intArr[arrSize] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,27,18,19,20};
 
 void replacEverySecondElementInAnArrayByZero(int a[]);
 void printArray(int a[]);
+void printOddNumSmallerThanTwenty (int array[], int len);
+void calcLargestFacWithUperLimit (int Limit);
 
 int main() {
     //a
-    cout<<"task a";
-    for (int i = 1; i < arrSize; ++i) {
-        printf("|%2d" , intArr[i]);
-    }
+    cout<<"task a (the 1st line shows the original array used before running the method)"<<endl;
+    printArray(intArr);
+    cout<<endl;
     replacEverySecondElementInAnArrayByZero(intArr);
-    //printArray(intArr);
-    for (int i = 1; i < arrSize; ++i) {
-        printf("|%2d" , intArr[i]);
-    }
+    printArray(intArr);
+
+    //b
+    cout<<endl<<endl<<"task b (the 1st line shows the original array used before running the method)"<<endl;
+    int intArr[arrSize] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,25,26,27,28,29,30};
+    printArray(intArr);
+    cout<<endl;
+    printOddNumSmallerThanTwenty(intArr, arrSize);
+
+    //c
+    cout<<endl<<endl<<"task c"<<endl;
+    calcLargestFacWithUperLimit (6);
+
     return 0;
 }
 
 // (a) Write a program that replaces every second element of an array by zero,
 // using a for loop and iterating with step size two.
 void replacEverySecondElementInAnArrayByZero (int intArr[]) {
-    for (int i = 1; i < arrSize; ++i) {
+    for (int i = 1; i < arrSize; i=i+2) {               //incerementing by 2
         intArr[i] = 0;
     }
 }
 
+//copied from exercise 4.2
 void printArray(int array[]){
-    for (int i = 0; i < arrSize; ++i) {       // rows
-            printf("|%2d" , array[i]);// printing out every element as a two digit number
+    for (int i = 0; i < arrSize; ++i) {         // rows
+        printf("|%2d" , array[i]);              // printing out every element as a two digit number
     }
     cout<<"|";
 }
 
 // (b) Write a program that prints out all odd numbers smaller than 20 by
 // using a loop and the continue statements.
+void printOddNumSmallerThanTwenty (int array[], int len){
+    for (int i = 0; i < len; ++i) {
+        if (array[i]%2 != 0 && array[i] < 20)  {            //
+            printf("|%2d", array[i]);
+            continue;                                       //resume the for loop
+        }
+    }
+    cout<<"|";
+}
 
 //(c) Write a program that calculates the largest factorial smaller than a given
 //upper limit. Therefore use a loop and the break statement to exit, when
 //the next number is larger than the limit.
+void calcLargestFacWithUperLimit (int Limit){
+
+}
 
 //(d) Binomial coeﬃcient
 //Write a program that computes the binomial coeﬃcient  (n k)
 //where n and k are speciﬁed by the user.
+
 
 //(e) Password prompt
 //Write a program that asks the user to enter a password. The user may
