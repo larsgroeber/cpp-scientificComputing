427b2593802b7e1a0fbf24b5b6bb34ae718881c9
Rudhorn <original.robert@web.de>
2016-12-02 11:35:06 +0100

    Update rec.cpp

--- exercises/sheet7/rec.cpp
+++ exercises/sheet7/rec.cpp
@@ -4,24 +4,35 @@ using namespace std;
 
 
 //defining the recursive function
-void recursion()
+void recursion(int c, double r, double y )
 {
-    int c = 0;     //a variable for counting the steps
-    double r = 0;  //parameter r from the exercise
-    double y;
+    // declare a filestream file
+    fstream file;
+    // open file, file name and writeable
+    file.open( "results_triangle_map.dat", ios_base::out );
+    c = 0;     //a variable for counting the steps
+    r = 0;  //parameter r from the exercise
     y=rand() %1;   //rolling a random y
     if (0 <= y );  //looking what value y is
         if ( c<500);    
             if (y<= 0.5);
-                    print y;
-                    y = r * y;
-                    c+=1;  //increasing our counter
-                    r=r+(2/500);
-                if (0.5 < y);
-                    if (y <= 1);
+                if (300<c);                       
+                    // write into file
+                    file << r << " " << y << endl;
+                    // also write to stdout
+                    std::cout << r << " " << y << endl;                    
+                y = r * y;      //making our new y
+                c+=1;           //increasing our counter
+                r=r+(2/500);    //increasing r
+            if (0.5 < y);
+                    if (y <= 1); //doing the same as above
+                        if (300<c);
+                            file << r << " " << y << endl;
+                            std::cout << r << " " << y << endl;                    
                         y = r - r * y;
                         c+=1;
                         r=r+(2/500);
+    file.close();
 }
 
 
@@ -29,5 +40,6 @@ void recursion()
 
 int main()
 {
-    cout << recursion();
+    recursion();
+    return 0;
 }
