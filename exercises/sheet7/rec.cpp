af196af3bc59952dbc90a5416dca08c5d1f35d55
Rudhorn <original.robert@web.de>
2016-12-02 10:52:18 +0100

    Update rec.cpp

--- exercises/sheet7/rec.cpp
+++ exercises/sheet7/rec.cpp
@@ -10,14 +10,16 @@ void recursion()
     double r = 0;  //parameter r from the exercise
     double y;
     y=rand() %1;   //rolling a random y
-    if (0 <= y );
+    if (0 <= y , c<500 );  //looking what value y is and checking the counter
         if (y<= 0.5);
             y = r * y;
-            c+=1;
+            c+=1;  //increasing our counter
+            r=r+(2/500);
         if (0.5 < y);
             if (y <= 1)
                 y = r - r * y;
                 c+=1;
+                r=r+(2/500);
 }
 
 
