ef3f56dbc3ad094de78d71a899f7b0318a83a897
Rudhorn <original.robert@web.de>
2016-12-16 11:08:11 +0100

    Update main.cpp

--- exercises/sheet9/p1-old/main.cpp
+++ exercises/sheet9/p1-old/main.cpp
@@ -1,29 +1,83 @@
-#include <iostream>
+#include <iostream>	// standard IO
+#include <stdio.h>	// printf()
 
-using namespace std;
+typedef std::ostream& (*STRFUNC)(std::ostream&);
 
-
-//defining the recursive function
-void recursion()
+template <size_t size>
+class BufferClass
 {
-    int c = 0;     //a variable for counting the steps
-    double r = 0;  //parameter r from the exercise
-    double y;
-    y=rand() %1;   //rolling a random y
-    if (0 <= y );
-    if (y<= 0.5);
-    y = r * y;
-    c+=1;
-    if (0.5 < y);
-    if (y <= 1)
-        y = r - r * y;
-    c+=1;
-}
+private:
+int pos;				     // for insertion
+double data[size];
 
+public:
+bool isEmpty,isFull;
 
+BufferClass<size>()			     // standard constructor
+  { pos=0;
+    isEmpty = true;
+    isFull = true;
+    if (size>0)
+      isFull = false;
+    for (int i=0;i<size;i++)
+      data[i] = 0;
+  }
 
+template <class T>
+BufferClass<size>& operator>>(T &outInt)	     // like cin >> variable
+  {
+  if (pos>0)
+    {
+    pos--;
+    isFull = false;
+    }
+  if (pos==0)
+    isEmpty = true;
+  outInt = data[pos];
+  return *this;
+  }
+
+template <class T>
+BufferClass<size>& operator<<(T newInt)	    // like cout << variable
+  {
+  if (!isFull)
+    {
+    data[pos] = newInt;
+    isEmpty = false;
+    }
+  if (pos<(size-1))
+    {
+    pos++;
+    }
+  else
+    isFull = true;
+  return *this;
+  }
+
+};
 
 int main()
 {
-    recursion();
+ BufferClass<10> myBuffer;	// a buffer for up-to 10 integers
+ double test = 0;
+//
+ printf("content of buffer: ");
+ while (!myBuffer.isEmpty)
+   {
+   myBuffer >> test;
+   printf("%4d",test);
+   }
+ printf("\n");
+//
+ myBuffer << 3.3;
+ myBuffer << 4 << 5;
+ printf("content of buffer: ");
+ while (!myBuffer.isEmpty)
+   {
+   myBuffer >> test;
+   printf("%4d",test);
+   }
+ printf("\n");
+//
+ return 0;
 }
